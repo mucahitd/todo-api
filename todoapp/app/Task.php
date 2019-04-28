1b68326ad1b6185a40de8ccf36d4273bc6a2df73
mucahitd <mucahitsec@gmail.com>
2019-04-28 19:48:12 +0300

    soft delete eklendi

---
+++ todoapp/app/Task.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SoftDelete extends Model
+{
+    use SoftDeletes;
+
+
+    protected $dates = ['deleted_at'];
+}
+
+
+class Task extends Model
+{
+    //
+}
---
+++ todoapp/database/migrations/2019_04_28_111030_add_soft_deletes_to_tasks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSoftDeletesToTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function ($table) {
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table("tasks", function ($table) {
+            $table->dropSoftDeletes();
+        });
+    }}
