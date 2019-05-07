2ceef354e27fbe016c5fb967bd083843955e2303
Mehmet Ali Cücen <mehmetalicucen@gmail.com>
2019-05-07 19:31:52 +0300

    fixed task model

--- todoapp/app/Task.php
+++ todoapp/app/Task.php
@@ -5,17 +5,14 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class SoftDelete extends Model
-{
-    use SoftDeletes;
-
-
-    protected $dates = ['deleted_at'];
-}
-
-
 class Task extends Model
 {
+    use SoftDeletes;
+    
+    protected $guarded = [];
+    
+    protected $dates = ['deleted_at'];
+    
     public function user()
     {
         return $this->belongsTo(User::class);
